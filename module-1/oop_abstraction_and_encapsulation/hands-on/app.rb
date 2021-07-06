a4dff46df6d8715bef6dd755d229c12502e141f9
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-06 22:00:09 +0700

    add handson

---
+++ module-1/oop_abstraction_and_encapsulation/hands-on/app.rb
@@ -0,0 +1,18 @@
+require_relative 'player'
+
+jin_sakai = Player.new("Jin Sakai", 100, 50)
+khotun_khan = Player.new("Khotun Khan", 500, 50)
+
+loop do 
+    jin_sakai.attacking_other(khotun_khan)
+    break if khotun_khan.is_die? 
+    puts
+
+    khotun_khan.attacking_other(jin_sakai)
+    break if jin_sakai.is_die? 
+    puts
+end
+
+puts
+puts "Jin Sakai dies" if jin_sakai.is_die?
+puts "Khotun Khan dies" if khotun_khan.is_die?
---
+++ module-1/oop_abstraction_and_encapsulation/hands-on/player.rb
@@ -0,0 +1,27 @@
+class Player
+    attr_reader :name #getter for state name
+    
+    def initialize(name, hitpoints, attack_damage)
+        @name = name
+        @hitpoints = hitpoints
+        @attack_damage = attack_damage
+    end
+
+    def to_s
+        return "#{@name} has #{@hitpoints} hitpoints and #{@attack_damage} attack"
+    end
+ 
+    def attacking_other(other_knight)
+        puts "#{@name} attack #{other_knight.name} with #{@attack_damage} points"
+        other_knight.attacked_by_others(@attack_damage)
+    end
+    
+    def attacked_by_others(damage)
+        @hitpoints -= damage
+        puts self
+    end
+ 
+    def is_die?
+        return @hitpoints <= 0
+    end
+end
