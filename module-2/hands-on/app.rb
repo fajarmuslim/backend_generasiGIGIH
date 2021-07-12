1688e66343c0d5bcbe922a349748fd5ad278c43d
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-12 18:35:46 +0700

    sinatra: templating

--- module-2/hands-on/app.rb
+++ module-2/hands-on/app.rb
@@ -5,14 +5,28 @@ get '/hello' do
   'hello world!'
 end
 
+# get '/messages' do
+#   "<h1 style='background-color:DodgerBlue;'>Hello world!</h1>"
+# end
+
 get '/messages' do
-  "<h1 style='background-color:DodgerBlue;'>Hello world!</h1>"
+  erb :message, locals: {
+    color: 'DodgerBlue',
+    name: 'fajar'
+  }
 end
 
+# get '/messages/:name' do
+#   name = params['name']
+#   color = params['color'] || 'DodgerBlue'
+#   "<h1 style='background-color:#{color};'>Hello #{name}!</h1>"
+# end
+#
 get '/messages/:name' do
-  name = params['name']
-  color = params['color'] || 'DodgerBlue'
-  "<h1 style='background-color:#{color};'>Hello #{name}!</h1>"
+  erb :message, locals: {
+    color: params['color'] || 'DodgerBlue',
+    name: params['name']
+  }
 end
 
 # they will redirect to '/messages/:name' . the rest of parameter is optional
