91966e6b7bd5c4baa078492eb13f5e01b4e842b0
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-12 18:00:11 +0700

    sinatria: using params

--- module-2/hands-on/app.rb
+++ module-2/hands-on/app.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+get '/hello' do
+  'hello world!'
+end
 
 get '/messages' do
-  'Hello world!'
+  "<h1 style='background-color:DodgerBlue;'>Hello world!</h1>"
 end
+
+get '/messages/:name' do
+  name = params['name']
+  color = params['color'] || 'DodgerBlue'
+  "<h1 style='background-color:#{color};'>Hello #{name}!</h1>"
+end
+
+# they will redirect to '/messages/:name' . the rest of parameter is optional
+# http://localhost:4567/messages/fajar?color=green&key=value
 
 # 404 Error!
 not_found do
