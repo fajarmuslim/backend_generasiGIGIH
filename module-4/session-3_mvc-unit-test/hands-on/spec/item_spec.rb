31c9e14c9c2f6b37b79816fa85a653a700fc17cb
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-30 18:33:36 +0700

    item: not valid input (price nil)

--- module-4/session-3_mvc-unit-test/hands-on/spec/item_spec.rb
+++ module-4/session-3_mvc-unit-test/hands-on/spec/item_spec.rb
@@ -11,20 +11,26 @@ describe Item do
                           price: 1000
                         })
 
-
         expect(item.valid?).to eq(true)
       end
 
-      it 'not valid input' do
+      it 'not valid input: name nil' do
         item = Item.new({
                           id: 1,
                           price: 1000
                         })
 
-
         expect(item.valid?).to eq(false)
       end
 
+      it 'not valid input: price nil' do
+        item = Item.new({
+                          id: 1,
+                          name: 'Nasi Uduk'
+                        })
+
+        expect(item.valid?).to eq(false)
+      end
 
     end
   end
