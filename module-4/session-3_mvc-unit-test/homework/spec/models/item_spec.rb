53bb3042ead7b918e205c74fcee2fe713ee74d60
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-31 09:50:53 +0700

    unit test: initialize item

--- module-4/session-3_mvc-unit-test/homework/spec/models/item_spec.rb
+++ module-4/session-3_mvc-unit-test/homework/spec/models/item_spec.rb
@@ -1,15 +1,17 @@
-require_relative '../../modelsitem'
+require_relative '../../models/item'
 require_relative '../../db/mysql_connector'
 
 describe Item do
   describe '#valid?' do
     context 'valid input' do
       it 'valid input' do
-        item = Item.new({
-                          id: 1,
-                          name: 'Nasi uduk',
-                          price: 1000
-                        })
+        params = {
+          id: 1,
+          name: 'Nasi uduk',
+          price: 1000
+        }
+
+        item = Item.new(params)
 
         expect(item.valid?).to eq(true)
       end
@@ -17,24 +19,46 @@ describe Item do
 
     context 'not valid input' do
       it 'name nil' do
-        item = Item.new({
-                          id: 1,
-                          price: 1000
-                        })
+        params = {
+          id: 1,
+          price: 1000
+        }
+
+        item = Item.new(params)
 
         expect(item.valid?).to be_falsey
       end
 
       it 'price nil' do
-        item = Item.new({
-                          id: 1,
-                          name: 'Nasi Uduk'
-                        })
+        params = {
+          id: 1,
+          name: 'Nasi Uduk'
+        }
+
+        item = Item.new(params)
 
         expect(item.valid?).to be_falsey
       end
     end
+  end
 
+  describe '.initialize' do
+    context 'valid input' do
+      it 'valid input' do
+        params = {
+          id: 1,
+          name: 'Nasi uduk',
+          price: 1000
+        }
+
+        item = Item.new(params)
+
+        expect(item.id).to eq(params [:id])
+        expect(item.name).to eq(params [:name])
+        expect(item.price).to eq(params [:price])
+        expect(item.categories).to eq([])
+      end
+    end
   end
 end
 
