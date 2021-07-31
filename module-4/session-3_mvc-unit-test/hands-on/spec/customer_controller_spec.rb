d70a277ddd8f4cd35811a36f3730f3363498f342
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-31 07:30:09 +0700

    customercontroller test

---
+++ module-4/session-3_mvc-unit-test/hands-on/spec/customer_controller_spec.rb
@@ -0,0 +1,56 @@
+require_relative '../controllers/customer_controller'
+require_relative '../db/mysql_connector'
+
+describe CustomerController do
+  describe '#show_all' do
+    context 'show all customer' do
+      it 'should show all customer view' do
+        controller = CustomerController.new
+
+        actual_view = controller.show_all
+
+        customers = Customer.find_all
+        expected_view = ERB.new(File.read('./views/customer/all_customer.erb')).result(binding)
+
+        expect(expected_view).to eq(actual_view)
+      end
+    end
+  end
+
+  describe 'save' do
+    context 'with valid object' do
+      it 'should save to db' do
+        params = {
+          name: 'Fajar Muslim',
+          phone: 8999999
+        }
+        customer = Customer.new(params)
+
+        mock_client = double
+        allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+
+        expect(mock_client).to receive(:query).with("INSERT INTO customers(name, phone) VALUES ('#{customer.name}', #{customer.phone})")
+        customer.save
+      end
+    end
+  end
+  # describe '#create_customer' do
+  #   context 'create customer success' do
+  #     it 'should create customer success' do
+  #       params = {
+  #         name: 'Fajar Muslim',
+  #         phone: 8999999
+  #       }
+  #
+  #       controller = CustomerController.new
+  #
+  #       response = controller.create_customer(params)
+  #
+  #       client = create_db_client
+  #       client.query("INSERT INTO customers(name, phone) VALUES ('#{params.name}', #{params.phone})")
+  #
+  #       expect(expected_view).to eq(actual_view)
+  #     end
+  #   end
+  # end
+end
--- module-4/session-3_mvc-unit-test/hands-on/spec/item_spec.rb
+++ module-4/session-3_mvc-unit-test/hands-on/spec/item_spec.rb
@@ -13,26 +13,28 @@ describe Item do
 
         expect(item.valid?).to eq(true)
       end
+    end
 
-      it 'not valid input: name nil' do
+    context 'not valid input' do
+      it 'name nil' do
         item = Item.new({
                           id: 1,
                           price: 1000
                         })
 
-        expect(item.valid?).to eq(false)
+        expect(item.valid?).to be_falsey
       end
 
-      it 'not valid input: price nil' do
+      it 'price nil' do
         item = Item.new({
                           id: 1,
                           name: 'Nasi Uduk'
                         })
 
-        expect(item.valid?).to eq(false)
+        expect(item.valid?).to be_falsey
       end
-
     end
+
   end
 end
 
