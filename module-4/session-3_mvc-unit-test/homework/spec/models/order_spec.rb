711e0fc1d190f9eb349bb00a787fede37e64bfc1
fajar muslim <13517149@std.stei.itb.ac.id>
2021-08-01 11:36:57 +0700

    unit test: save order

--- module-4/session-3_mvc-unit-test/homework/spec/models/order_spec.rb
+++ module-4/session-3_mvc-unit-test/homework/spec/models/order_spec.rb
@@ -60,4 +60,25 @@ describe Item do
       end
     end
   end
+
+  describe '#save' do
+    context 'valid input' do
+      it 'should save data to db' do
+        params = {
+          id: 1,
+          customer_id: 1,
+          date: '2021-07-01 01:01:01',
+          total_price: 52000
+        }
+
+        order = Order.new(params)
+
+        mock_client = double
+        allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+
+        expect(mock_client).to receive(:query).with("INSERT INTO orders(customer_id, date, total_price) VALUES (#{params[:customer_id]}, '#{params[:date]}', #{params[:total_price]})")
+        order.save
+      end
+    end
+  end
 end
