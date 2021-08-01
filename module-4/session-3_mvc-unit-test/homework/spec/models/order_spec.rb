09a8f195829192a306e699c041cdc4db15bc75bd
fajar muslim <13517149@std.stei.itb.ac.id>
2021-08-01 11:55:36 +0700

    unit test: find all order

--- module-4/session-3_mvc-unit-test/homework/spec/models/order_spec.rb
+++ module-4/session-3_mvc-unit-test/homework/spec/models/order_spec.rb
@@ -81,4 +81,39 @@ describe Item do
       end
     end
   end
+
+  describe '.find_all' do
+    context 'find all orders' do
+      it 'should returning all orders from db' do
+        query_result_mock = [
+          { "id" => 1, "customer_id" => 1, "date" => '2021 - 07 - 01 01 : 01 : 01', "total_price" => 52000 },
+          { "id" => 2, "customer_id" => 1, "date" => '2021 - 07 - 01 02 : 02 : 02', "total_price" => 71000 },
+          { "id" => 3, "customer_id" => 2, "date" => '2021 - 07 - 01 03 : 03 : 03', "total_price" => 72000 },
+        ]
+
+        mock_client = double
+        allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+        allow(mock_client).to receive(:query).with('SELECT * FROM orders').and_return(query_result_mock)
+
+        actual_result = Order.find_all
+        expected_order_1 = Order.new({ id: 1, customer_id: 1, date: '2021 - 07 - 01 01 : 01 : 01', total_price: 52000 })
+        expected_order_2 = Order.new({ id: 2, customer_id: 1, date: '2021 - 07 - 01 02 : 02 : 02', total_price: 71000 })
+        expected_order_3 = Order.new({ id: 3, customer_id: 2, date: '2021 - 07 - 01 03 : 03 : 03', total_price: 72000 })
+
+        expected_result = [
+          expected_order_1,
+          expected_order_2,
+          expected_order_3
+        ]
+
+        expect(expected_result.size).to eq(actual_result.size)
+        (0..expected_result.size - 1).each do |i|
+          expect(actual_result[i].id).to eq(expected_result[i].id)
+          expect(actual_result[i].customer_id).to eq(expected_result[i].customer_id)
+          expect(actual_result[i].date).to eq(expected_result[i].date)
+          expect(actual_result[i].total_price).to eq(expected_result[i].total_price)
+        end
+      end
+    end
+  end
 end
